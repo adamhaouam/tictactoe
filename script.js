7f33877970588114104b3bf7be99956f4e9bffee
Adam H <183345857+adamhaouam@users.noreply.github.com>
2025-10-01 22:46:24 +1000

    create factory function for gameboard and squares

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("sup world");
+function Gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let i = 0; i < columns; i++) {
+            board[i].push(Square());
+        }
+    }
+}
+
+function Square() {
+    let value = 0;
+
+    const setSquare = (player) => {
+        value = player;
+    }
+    const getSquare = () => value;
+
+    return {
+        setSquare,
+        getSquare
+    };
+}

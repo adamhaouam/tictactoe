91e77c780f2e2b0c340ad14a9457a2e41cfe5251
Adam H <183345857+adamhaouam@users.noreply.github.com>
2025-10-12 17:37:47 +1100

    add printBoard to Gameboard
    prints current board in console

--- script.js
+++ script.js
@@ -12,8 +12,14 @@ function Gameboard() {
 
     const getBoard = () => board;
 
+    const printBoard = () => {
+		const boardWithCellValues = board.map((row) => row.map((cell) => cell.getSquare()).join(', ')).join('\n');
+		console.log(boardWithCellValues);
+	};
+
     return {
-        getBoard
+        getBoard,
+        printBoard
     }
 }
 
